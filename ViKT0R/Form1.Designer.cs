11d245105e83059988b273af306d021d271ce96e
George Delaportas <g.delaportas@gmail.com>
2018-11-24 10:29:23 +0200

    Initial commit

---
+++ ViKT0R/Form1.Designer.cs
@@ -0,0 +1,81 @@
+﻿namespace ViKT0R
+{
+    partial class VForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.textBox1.Cursor = System.Windows.Forms.Cursors.No;
+            this.textBox1.ForeColor = System.Drawing.Color.Lime;
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.ShortcutsEnabled = false;
+            this.textBox1.Size = new System.Drawing.Size(580, 323);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(263, 341);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(82, 24);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "START";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // VForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(604, 372);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "VForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "ViKT0R - SysLog (UDP Scanner)";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.VForm_FormClosed);
+            this.Load += new System.EventHandler(this.VForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ ViKT0R/Form1.cs
@@ -0,0 +1,92 @@
+﻿/*
+    ViKT0R - Syslog scanner
+    Copyright (C) - 2018
+    Coded by George Delaportas (G0D/ViR4X)
+*/
+
+
+
+using System;
+using System.IO;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace ViKT0R
+{
+    public partial class VForm : Form
+    {
+        private Thread New_Thread;
+        private UdpClient UDP_Listener;
+        private IPEndPoint End_Points;
+        private bool isRunning = false;
+
+        public VForm()
+        {
+            InitializeComponent();
+        }
+
+        private void VForm_Load(object sender, EventArgs e)
+        {
+            //timer1.Enabled = true;
+        }
+
+        private void scanUDP()
+        {
+            isRunning = true;
+
+            UDP_Listener = new UdpClient(514);
+            End_Points = new IPEndPoint(IPAddress.Any, 514);
+
+            try
+            {
+                while (true)
+                {
+                    byte[] bytes = UDP_Listener.Receive(ref End_Points);
+
+                    textBox1.AppendText("[SOURCE]" + Environment.NewLine);
+                    textBox1.AppendText(End_Points.ToString() + Environment.NewLine + Environment.NewLine);
+                    textBox1.AppendText("[MESSAGE]" + Environment.NewLine);
+                    textBox1.AppendText(Encoding.ASCII.GetString(bytes, 0, bytes.Length) + 
+                                        Environment.NewLine + Environment.NewLine + Environment.NewLine);
+
+                    File.AppendAllText(Application.StartupPath + "/ViKT0R.log", textBox1.Text + Environment.NewLine);
+
+                    Thread.Sleep(1000);
+                }
+            }
+            catch (Exception ex)
+            {
+                if (ex.HResult != -2146233040)
+                    MessageBox.Show(ex.Message.ToString(), "ViKT0R");
+            }
+            finally
+            {
+                UDP_Listener.Close();
+            }
+        }
+
+        private void VForm_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            if (isRunning)
+            {
+                New_Thread.Abort();
+
+                UDP_Listener.Close();
+
+                Application.ExitThread();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            New_Thread = new Thread(scanUDP);
+
+            New_Thread.Start();
+
+            button1.Enabled = false;
+        }
+    }
+}
